9ee1a9e70afcabce98d46097ae3a5ef3ee14b7eb
Natasja Vitoft <natasjavitoft@outlook.dk>
2023-12-18 10:33:51 +0100

    alt muligt

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export function Navibar() {
+    return (
+        <>
+            <nav className="nav">
+                <a href="/" className="site-title"> Forside </a>
+                <ul>
+                    <li>
+                    <a href="/login">Login</a>
+                    </li>
+                    <li>
+                    <a href="/Indkøbskurv">Indkøbskurv</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
